e4f45761c9437f36a03103ff4173ccbba4ccc0e6
brdavis330 <125531743+brdavis330@users.noreply.github.com>
2023-05-10 19:29:02 +0000

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
-
-
+  root to: "application#root"
 
   # Routes for the Message resource:
 
   # CREATE
   post("/insert_message", { :controller => "messages", :action => "create" })
-          
+
   # READ
   get("/messages", { :controller => "messages", :action => "index" })
-  
+
   get("/messages/:path_id", { :controller => "messages", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_message/:path_id", { :controller => "messages", :action => "update" })
-  
+
   # DELETE
   get("/delete_message/:path_id", { :controller => "messages", :action => "destroy" })
 
@@ -25,16 +24,16 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_ticket", { :controller => "tickets", :action => "create" })
-          
+
   # READ
   get("/tickets", { :controller => "tickets", :action => "index" })
-  
+
   get("/tickets/:path_id", { :controller => "tickets", :action => "show" })
-  
+
   # UPDATE
-  
+
   post("/modify_ticket/:path_id", { :controller => "tickets", :action => "update" })
-  
+
   # DELETE
   get("/delete_ticket/:path_id", { :controller => "tickets", :action => "destroy" })
 
@@ -43,15 +42,15 @@ Rails.application.routes.draw do
   # Routes for the User account:
 
   # SIGN UP FORM
-  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })        
+  get("/user_sign_up", { :controller => "user_authentication", :action => "sign_up_form" })
   # CREATE RECORD
-  post("/insert_user", { :controller => "user_authentication", :action => "create"  })
-      
-  # EDIT PROFILE FORM        
-  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
+  post("/insert_user", { :controller => "user_authentication", :action => "create" })
+
+  # EDIT PROFILE FORM
+  get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })
   # UPDATE RECORD
   post("/modify_user", { :controller => "user_authentication", :action => "update" })
-  
+
   # DELETE RECORD
   get("/cancel_user_account", { :controller => "user_authentication", :action => "destroy" })
 
@@ -61,10 +60,10 @@ Rails.application.routes.draw do
   get("/user_sign_in", { :controller => "user_authentication", :action => "sign_in_form" })
   # AUTHENTICATE AND STORE COOKIE
   post("/user_verify_credentials", { :controller => "user_authentication", :action => "create_cookie" })
-  
-  # SIGN OUT        
+
+  # SIGN OUT
   get("/user_sign_out", { :controller => "user_authentication", :action => "destroy_cookies" })
-             
+
   #------------------------------
 
 end
